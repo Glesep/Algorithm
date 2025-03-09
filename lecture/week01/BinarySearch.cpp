6c6b48d7fc51a4d364c6ad3946a27ba3ec650f31
Glesep <yskite09@gmail.com>
2025-03-09 20:34:43 +0900

    이진검색 코드 추가

---
+++ lecture/week01/BinarySearch.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int n, char *data[], char *target) {
+    int begin = 0, end = n-1;
+
+    while(begin <= end) {
+        int middle = (begin + end) / 2;
+        int result = strcmp(data[middle], target);
+
+        if (result == 0)
+            return middle;
+        else if (result < 0)
+            begin = middle+1;
+        else
+            end = middle-1;
+    }
+
+    return -1;
+}
