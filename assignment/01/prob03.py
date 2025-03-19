fd0ba4b7e28cccb0e5d801eb897026d5aed92ce2
Glesep <yskite09@gmail.com>
2025-03-19 11:39:12 +0900

    3번 풀이 완료

--- assignment/01/prob03.py
+++ assignment/01/prob03.py
@@ -1,6 +1,6 @@
 # 변수 입력 받기
 N=int(input()) # N개의 행
-arr=[[*map(int,input().split())] for _ in range(N)]
+maze=[[*map(int,input().split())] for _ in range(N)]
 
 K = int(input())
 
@@ -26,13 +26,42 @@ def moveable(x, y, dir):
     x_next = x + offset[dir][0]
     y_next = y + offset[dir][1]
     
-    return (x_next >= 0 &
-            x_next < N &
-            y_next >= 0 &
-            y_next < N &
-            arr[x_next][y_next] == 0)
+    return (x_next >= 0 and
+            x_next < N and
+            y_next >= 0 and
+            y_next < N and
+            maze[x_next][y_next] == 0)
     
-def moveto(x, y, dir):
-    
+# 경로 개수
+route = 0
 
-def solveMaze(x, y):
+def solveMaze(x, y, dir, move_count):
+    """미로의 경로를 구하는 함수입니다.
+
+    Args:
+        x (int): 현재 위치의 x좌표
+        y (int): 현재 위치의 y좌표
+        dir (int): 가고 싶은 방향
+        move_count (int): 이동 횟수
+    """
+    global route
+    maze[x][y] = 2          # visited 표시
+    
+    if move_count <= K:      # 조건 충족
+        
+        if x == N-1 and y == N-1:   # 목적지 도착
+            route = route+1
+        
+        else:
+            while (dir < 4):
+                
+                if moveable(x, y, dir):     # 움직일 수 있으면
+                    solveMaze(x+offset[dir][0], y+offset[dir][1], 0, move_count+1)
+                    
+                dir = dir + 1   # 방향 바꾸기
+        
+    maze[x][y] = 0  # 자신이 온 흔적 없애기 - 다른 경우의 수에 방해가 될 수 있으므로
+
+solveMaze(0, 0, 0, 0)
+    
+print(route)
