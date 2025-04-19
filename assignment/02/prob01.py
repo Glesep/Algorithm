0329766400474b9058c5e747341ab0fb68f5c015
Glesep <yskite09@gmail.com>
2025-04-19 12:41:59 +0900

    heap_sort 완성판

--- assignment/02/prob01.py
+++ assignment/02/prob01.py
@@ -33,12 +33,12 @@ def insertion_sort(words_copy):
     Returns:
         list: 정렬된 리스트
     """
-    for i in range(1, len(words_copy)-1):
+    for i in range(1, len(words_copy)):
         tmp = words_copy[i]
-        j = i - 1
+        j = i
         
-        while j > 0 and tmp < words_copy[j]:
-            words_copy[j+1] = words_copy[j]
+        while j > 0 and tmp < words_copy[j-1]:
+            words_copy[j] = words_copy[j-1]
             j -= 1
             
         words_copy[j] = tmp   
@@ -146,6 +146,8 @@ def quick_sort(words_copy, p, r):
     return words_copy
     
 # -------------------------------------------------------------------------------------------------------
+# max-heapify의 시간복잡도는 트리의 높이를 넘을 수 없음
+# 따라서 O(logn)
 def max_heapify(words_copy, i):
     """노드 i를 루트로 하는 서브트리를 heapify </br>
     
@@ -160,20 +162,24 @@ def max_heapify(words_copy, i):
         list: 분할 정복 기법으로 heapify가 완료된 words_copy 리스트
     """
     
+    # 자식 노드가 없다면 종료
     if (i*2+1 > len(words_copy)-1):
         return words_copy
     
-    
+    # 자식 노드 중 큰 쪽을 선택
     k = i*2+1 if (i*2+2 > len(words_copy)-1) or (words_copy[i*2+1] > words_copy[i*2+2]) else i*2+2
     
+    # 만약 선택된 자식 노드보다 부모노드가 크거나 같다면 종료
     if (words_copy[i] >= words_copy[k]):
         return words_copy
 
+    # 아니라면 교환
     words_copy[i], words_copy[k] = words_copy[k], words_copy[i]
     
+    # 후 교환된 노드를 root로 하는 max heapify를 recursive하게 실행
     return max_heapify(words_copy, k)
     
-
+# O(n)으로 알려져있다.
 def build_max_heap(words_copy):
     """words_copy 리스트 구성을 max-heap으로 만드는 함수입니다.
 
@@ -189,14 +195,15 @@ def build_max_heap(words_copy):
         
     return words_copy
 
-def heap_sort(words_copy):
-    """heap sort 알고리즘을 구현한 함수입니다.
+def max_heap_sort(words_copy):
+    """ max-heap으로 heap sort 알고리즘을 구현한 함수입니다.
+        오름차순 정렬이 수행됩니다.
 
     Args:
         words_copy (list): 정렬하고 싶은 리스트
 
     Returns:
-        list: heap sort로 정렬이 된 리스트
+        list: heap sort로 오름차순 정렬이 된 리스트
     """
     
     words_copy = build_max_heap(words_copy)
@@ -208,17 +215,120 @@ def heap_sort(words_copy):
         
     return words_copy
         
+# -------------------------------------------------------------------------------------------------------
+ 
+def min_heapify(words_copy, i):
+    """노드 i를 루트로 하는 서브트리를 min-heapify </br>
     
-archive = {}
+    if i가 부모 노드라면, 자식노드는 2i+1, 2i+2</br></br>
+    전체 트리의 루트 노드를 0번 노드라고 가정합니다.
+
+    Args:
+        words_copy (list): string으로 구성된 list
+        i (int): 서브트리의 루트로 삼고 싶은 노드
+
+    Returns:
+        list: 분할 정복 기법으로 heapify가 완료된 words_copy 리스트
+    """
+    
+    # 자식 노드가 없다면 종료
+    if (i*2+1 > len(words_copy)-1):
+        return words_copy
+    
+    # 자식 노드 중 작은 쪽을 선택
+    k = i*2+1 if (i*2+2 > len(words_copy)-1) or (words_copy[i*2+1] < words_copy[i*2+2]) else i*2+2
+    
+    # 만약 선택된 자식 노드보다 부모노드가 작거나 같다면 종료
+    if (words_copy[i] <= words_copy[k]):
+        return words_copy
+
+    # 아니라면 교환
+    words_copy[i], words_copy[k] = words_copy[k], words_copy[i]
+    
+    # 후 교환된 노드를 root로 하는 min heapify를 recursive하게 실행
+    return min_heapify(words_copy, k)
+
+# O(n)으로 알려져있다.
+def build_min_heap(words_copy):
+    """words_copy 리스트 구성을 min-heap으로 만드는 함수입니다.
+
+    Args:
+        words_copy (list): min-heap으로 구성하고 싶은 list
+
+    Returns:
+        list: min-heap화 완료된 리스트
+    """
+    
+    for i in range(len(words_copy)//2, -1, -1):
+        words_copy = min_heapify(words_copy, i)
+        
+    return words_copy
+
+def min_heap_sort(words_copy):
+    """ min-heap으로 heap sort 알고리즘을 구현한 함수입니다.
+        내림차순 정렬이 수행됩니다.
+
+    Args:
+        words_copy (list): 정렬하고 싶은 리스트
+
+    Returns:
+        list: heap sort로 내림차순 정렬이 된 리스트
+    """
+    
+    words_copy = build_min_heap(words_copy)
+    heap_size = len(words_copy)
+    while heap_size > 1:
+        words_copy[heap_size-1], words_copy[0] = words_copy[0], words_copy[heap_size-1]
+        heap_size -= 1
+        words_copy[:heap_size] = min_heapify(words_copy[:heap_size], 0)
+        
+    return words_copy
+
+# -------------------------------------------------------------------------------------------------------
+
+# max, min heap_sort를 합친 함수를 구현함
+def heap_sort(words_copy, ascending=True):
+    
+    if ascending:
+        words_copy = build_max_heap(words_copy)
+        heap_size = len(words_copy)
+        while heap_size > 1:
+            words_copy[heap_size-1], words_copy[0] = words_copy[0], words_copy[heap_size-1]
+            heap_size -= 1
+            words_copy[:heap_size] = max_heapify(words_copy[:heap_size], 0)
+
+        
+    
+    else:
+        words_copy = build_min_heap(words_copy)
+        heap_size = len(words_copy)
+        while heap_size > 1:
+            words_copy[heap_size-1], words_copy[0] = words_copy[0], words_copy[heap_size-1]
+            heap_size -= 1
+            words_copy[:heap_size] = min_heapify(words_copy[:heap_size], 0)
+
+
+    return words_copy
+
+# -------------------------------------------------------------------------------------------------------
+# 자유작성
+
+target_list = [10,20,9,81,33,7,100]
+
+result = heap_sort(target_list, True)
+print(result)
+
+# -------------------------------------------------------------------------------------------------------
+# archive = {}
 
 # sort들을 각각 실행하는 것은 측정이 되는데 한꺼번에 실행하는 것은 stackoverflow가 납니다.
 
 # 380.6092691421509s
-words_copied = words
-start_time = time.time()
-bubble_sort(words_copied)
-end_time = time.time()
-archive['bubble_sort'] = end_time-start_time
+# words_copied = words
+# start_time = time.time()
+# bubble_sort(words_copied)
+# end_time = time.time()
+# archive['bubble_sort'] = end_time-start_time
 
 
 # 239.17551016807556s
@@ -263,4 +373,4 @@ archive['bubble_sort'] = end_time-start_time
 # end_time = time.time()
 # archive['library_sort'] = end_time-start_time
 
-print(archive)
+# print(archive)
