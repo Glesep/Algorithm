73d8008bab2b41a5449d0503e29dfea324736654
Glesep <yskite09@gmail.com>
2025-05-22 14:56:04 +0900

    개인과제 04 완료

--- assignment/04/prob01.py
+++ assignment/04/prob01.py
@@ -2,60 +2,29 @@
 # 가짜 텍스트의 최대 길이는 100단어로 하라.
 import random
 
-
-
-
-
-def make_random_text(first, second, result='', length=0):
-    
-    tuple_target = (first, second)
-    
-    # [end]가 suffix인 경우
-    if dict_Markov[tuple_target][0] == '[end]':
-        return result
-    
-    if len(dict_Markov[tuple_target]) > 1:  # value list 내 요소가 2개 이상인 경우
-        index = [idx for idx in range(len(dict_Markov[tuple_target]))]
-        
-        target_word = ''
-        while index:
-            random_index = index.pop(random.choice(index))
-            
-            if len(result + ' ' + dict_Markov[tuple_target][random_index]) > 100:
-                continue
-            else:
-                target_word = dict_Markov[tuple_target][random_index]
-                break
-            
-        if target_word == '':   # 랜덤 텍스트의 길이가 100이 넘는 경우
-            return result
-      
-    else:       # value list 내 요소가 2개 이상인 경우
-        target_word = dict_Markov[tuple_target][0]
-        
-    return make_random_text(second, target_word, result+' '+target_word, len(result+' '+target_word))
-                
-
-# ========================================================================================================================
-if __name__ == "__main__":
-
+def prepare_data(file):
     dict_Markov = dict()
     # 데이터 전처리
-    with open('./files/HarryPotter.txt', 'r') as f:
+    with open(f'./files/{file}', 'r') as f:
         harry = f.readlines()
 
-        # 데이터 정리
+        # 데이터 정리 (문장 집합 -> 리스트)
         for i in range(len(harry)):
-            harry[i] = harry[i].strip().split('\t')
-        harry = [line for sublist in harry for line in sublist if line != '']
+            harry[i] = harry[i].strip().split('\t')     # 문장 앞뒤 공백 제거 후 '\t' 이준 슬라이스
+        harry = [line for sublist in harry for line in sublist if line != '']   # '' 삭제
 
+        # 리스트 타입으로 묶인 단어들을 문자열 타입 변수에 이어 붙임
         script = ''
-        for line in harry:
+        for line in harry:  
             script += line + ' '
 
+        # 문자열을 공백 기준으로 슬라이싱
         words = script.split()
+        
+        # [end] 추가
         words.append('[end]')
 
+    # Markov 체인을 이용한 데이터셋 준비
     first = ''
     second = ''
 
@@ -80,20 +49,57 @@ if __name__ == "__main__":
                 dict_Markov[tuple_words] = [word]
 
             # 앞으로 하나씩 밀기
-            # if words
             first = second
             second = word
+            
+    return dict_Markov
 
-    print(dict_Markov.keys())
+def make_random_text(first, second, result='', length=0):
+    
+    if result == '':
+        result = first + ' ' + second
+    
+    tuple_target = (first, second)
+    
+    # [end]가 suffix인 경우
+    if dict_Markov[tuple_target][0] == '[end]':
+        return result
+    
+    if len(dict_Markov[tuple_target]) > 1:  # value list 내 요소가 2개 이상인 경우
+        index = [idx for idx in range(len(dict_Markov[tuple_target]))]
+        
+        target_word = ''
+        while index:
+            random_index = index.pop(random.randrange(len(index)))      # pop의 매개변수는 리스트의 인덱스를 받음
+            
+            if len(result + ' ' + dict_Markov[tuple_target][random_index]) > 100:
+                continue
+            else:
+                target_word = dict_Markov[tuple_target][random_index]
+                break
+            
+        if target_word == '':   # 랜덤 텍스트의 길이가 100이 넘는 경우
+            return result
+      
+    else:       # value list 내 요소가 2개 이상인 경우
+        target_word = dict_Markov[tuple_target][0]
+        if len(result + ' ' + target_word) > 100:
+            return result
+
+    return make_random_text(second, target_word, result+' '+target_word, len(result+' '+target_word))
+                
+
+# ========================================================================================================================
+if __name__ == "__main__":
+
+    dict_Markov = prepare_data('HarryPotter.txt')
+
+    # print(dict_Markov.keys())     #무슨 키가 있는지 확인 (debug용)
     input_str = input()
     
     input_first, input_second = input_str.split()
 
-    
-    # print(words)
-    # print(('did.', '[end]') in dict_Markov.keys())
     result = make_random_text(input_first, input_second)       
     
     print(result)
-    
-
+    # print(len(result))      # 결과의 길이 확인 (debug용)
